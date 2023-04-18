94a59a3dd730934ab3ff9cb93467fc1cbcca6d5c
ND-SR7 <davogic.sr7.2021@uns.ac.rs>
2023-04-18 16:16:11 +0200

    Inicijalizacija projekta
    -Inicijalizovan projekat koristeći Android Studio
    -Dodate aktivnosti MainActivity i SplashScreenActivity (prazne)

---
+++ app/src/main/java/com/ftn/ac/rs/mobilne_2023/activities/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.ftn.ac.rs.mobilne_2023.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ftn.ac.rs.mobilne_2023.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
