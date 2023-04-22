28d2bd3080a623ff5ad2cce8208bbe874330eb57
ND-SR7 <davogic.sr7.2021@uns.ac.rs>
2023-04-22 17:40:35 +0200

    Stilizacija splash-a
    Dodata stilizacija SplashScreenActivity-a

--- app/src/main/java/com/ftn/ac/rs/mobilne_2023/activities/SplashScreenActivity.java
+++ app/src/main/java/com/ftn/ac/rs/mobilne_2023/activities/SplashScreenActivity.java
@@ -2,15 +2,38 @@ package com.ftn.ac.rs.mobilne_2023.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.ftn.ac.rs.mobilne_2023.MainActivity;
 import com.ftn.ac.rs.mobilne_2023.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashScreenActivity extends AppCompatActivity {
+
+    public Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash_screen);
+        int SPLASH_TIME_OUT = 3000;
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        timer.cancel();
     }
 }
