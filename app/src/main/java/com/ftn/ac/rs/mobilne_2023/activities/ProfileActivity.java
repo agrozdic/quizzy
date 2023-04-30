2d96241a617b9368e13cd327acb56f05dddbcf3d
Aleksandar Grozdić <agrozdic08@gmail.com>
2023-04-30 03:33:58 +0200

    Dodata aktivnost Profile

---
+++ app/src/main/java/com/ftn/ac/rs/mobilne_2023/activities/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.ftn.ac.rs.mobilne_2023.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ftn.ac.rs.mobilne_2023.R;
+import com.ftn.ac.rs.mobilne_2023.fragments.GameHeaderFragment;
+import com.ftn.ac.rs.mobilne_2023.tools.FragmentTransition;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_profile);
+    }
+}
