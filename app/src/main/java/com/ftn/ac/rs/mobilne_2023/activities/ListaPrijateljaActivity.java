34ee210e1671c65326de2d688eb65ead3caf8bc4
Aleksandar Grozdić <agrozdic08@gmail.com>
2023-04-30 04:02:46 +0200

    Dodata aktivnost ListaPrijatelja

---
+++ app/src/main/java/com/ftn/ac/rs/mobilne_2023/activities/ListaPrijateljaActivity.java
@@ -0,0 +1,17 @@
+package com.ftn.ac.rs.mobilne_2023.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ftn.ac.rs.mobilne_2023.R;
+
+public class ListaPrijateljaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_lista_prijatelja);
+    }
+}
