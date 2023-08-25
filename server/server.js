5bf2dca41ca5aa23af861fbb9ce417f95251b067
ND-SR7 <davogic.sr7.2021@uns.ac.rs>
2023-08-25 12:48:10 +0200

    Ko zna zna - 2 igrača
    -Implementirana igra ko zna zna za 2 igrača
    -Dodato slanje imena igrača serveru, kao i njihovo postavljanje u partiji

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
+// model
+class KoZnaZnaAnswer {
+  constructor(correct, timeOf, player) {
+      this.correct = correct;
+      this.timeOf = timeOf;
+      this.player = player;
+  }
+}
+
 // inicijalizacija express-a i pravljenje servera
 const app = express();
 const server = http.createServer(app);
@@ -15,13 +24,16 @@ const io = socketIO(server, {
 
 // pokretanje servera
 const port = 3000;
-server.listen(port, '192.168.1.3', () => {
+server.listen(port, '192.168.1.6', () => {
   console.log(`Server running on port ${port}`);
 });
 
+// podaci za igru
 let players = [];
+let playerSocket = {};
 let scores = {};
 let currentTurn = 0;
+let koZnaZnaAnswers = [];
 
 app.use(express.static('public'));
 
@@ -34,44 +46,75 @@ io.on('connection', (socket) => {
   socket.on('joinGame', (playerName) => {
     if (players.length < 2) {
       players.push(playerName);
+      playerSocket[socket.id] = playerName;
       scores[playerName] = 0;
+      console.log('Player joined: ' + playerName + '. Total players: ' + players.length);
       io.emit('playerJoined', playerName);
 
       if (players.length === 2) {
-        io.emit('startGame');
+        io.emit('startGame', playerSocket);
       }
     }
   });
 
-  // TODO - samo red i bodove da vraca
-  socket.on('answerQuestion', (answer) => {
-    const currentPlayer = players[currentTurn];
-    if (currentPlayer === socket.id) {
-      // Check if the answer is correct
-      // (Implement your own logic here)
+  // ko zna zna
+  socket.on('koZnaZnaAnswer', (correct, timeOf, player) => {
+    let answer = new KoZnaZnaAnswer(correct, timeOf, player);
+    koZnaZnaAnswers.push(answer);
+  });
 
-      // Update the score
-      if (answerIsCorrect) {
-        scores[currentPlayer]++;
+  socket.on('koZnaZnaAnswerCheck', async () => {
+    if (koZnaZnaAnswers.length === 2) {
+      console.log(koZnaZnaAnswers);
+      let answer1 = koZnaZnaAnswers[0];
+      let answer2 = koZnaZnaAnswers[1];
+
+      if ((answer1.correct && answer2.correct && answer1.timeOf < answer2.timeOf)
+          || (answer1.correct && !answer2.correct)) {
+        scores[answer1.player] += 10;
+      } else if (answer2.correct && answer1.correct && answer2.timeOf < answer1.timeOf
+        || (answer2.correct && !answer1.correct)) {
+        scores[answer2.player] += 10;
       }
 
-      // Increment the turn
-      currentTurn = (currentTurn + 1) % 2;
+      if (!answer1.correct) {
+        scores[answer1.player] -= 5;
+      }
 
-      // Emit the updated scores and current turn
+      if (!answer2.correct) {
+        scores[answer2.player] -= 5;
+      }
+
       io.emit('scoreUpdate', scores);
-      io.emit('turnChange', currentTurn);
+      console.log(scores);
+      koZnaZnaAnswers = [];
+    } else if (koZnaZnaAnswers.length === 1) {
+      console.log(koZnaZnaAnswers);
+      let answer1 = koZnaZnaAnswers[0];
+
+      if (answer1.correct) {
+        scores[answer1.player] += 10;
+      }
+
+      if (!answer1.correct) {
+        scores[answer1.player] -= 5;
+      }
+
+      io.emit('scoreUpdate', scores);
+      console.log(scores);
+      koZnaZnaAnswers = [];
     }
   });
 
+  // diskonekcija
   socket.on('disconnect', () => {
     console.log('A user disconnected');
-    const index = players.indexOf(socket.id);
-    if (index !== -1) {
-      const playerName = players[index];
-      players.splice(index, 1);
-      delete scores[playerName];
-      io.emit('playerLeft', playerName);
-    }
+
+    const name = playerSocket[socket.id];
+    const index = players.indexOf(name);
+    players.splice(index, 1);
+    delete scores[name];
+
+    io.emit('playerLeft', name);
   });
 });
